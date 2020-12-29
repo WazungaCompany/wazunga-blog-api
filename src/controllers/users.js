b80bfd3ef3d1a1119e569179cba84646e6e97524
Sergio Alberto Bernal Cisneros <sergiobernal909@gmail.com>
2020-12-29 17:23:23 -0600

    UpdateUser done

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -46,15 +46,24 @@ exports.getUsers = function (req, res) {
  * @param {Response} res
  */
 exports.updateUser = async function (req, res) {
-    const user = req.body;
-    console.log(Object.keys(user).length);
-    if (Object.keys(user).length === 0 || user.constructor === {})
+    const nUser = req.body;
+    // Verificar si el req.body contiene las propiedades del user
+    if (Object.keys(nUser).length <= 1 || nUser.constructor === {})
         return badResponse(res, { user: { msg: 'No fueron enviados los parametros necesarios' } });
-    const { user_id } = user;
+    const { user_id } = nUser;
+    console.log('user_id: ', user_id);
+    // Verificar si el id del usuario fu enviado
     if (!user_id)
         return badResponse(res, { user_id: { msg: 'No se recibio el id del usuario' } });
     const userExists = await existsRegister(User, user_id);
+    console.log('userExists', userExists);
+    // Verificar si el usuario existe
     if (!userExists)
         return notFoundResponse(res, `User -> ${user_id}`); 
-    return successResponse(res, "works")
+    User.findByIdAndUpdate(user_id, nUser, { new: true }, (err, result) => {
+        if(err)
+            return internalServerErrorResponse(res, err);
+        // Enviar el usuario actualizado
+        return successResponse(res, result);
+    });
 }
