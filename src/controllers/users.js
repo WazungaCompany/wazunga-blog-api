29025bc9d0c6e8a492fab03e177ecc6392f4603e
Sergio Alberto Bernal Cisneros <sergiobernal909@gmail.com>
2020-12-28 19:45:26 -0600

    Params refactoring

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,11 @@
 const User = require("../models/users");
 const { validationError } = require("../utils/mongooseErrorsHandler");
-const { successRequest, badRequest } = require("../utils/reponsesHandler");
+const { 
+    successResponse,
+    badResponse,
+    notFoundResponse,
+    internalServerErrorResponse
+} = require("../utils/reponsesHandler");
 
 /** Para agregar un usuario se reciben las propiedades
  * de este atraves del body.
@@ -9,13 +14,27 @@ const { successRequest, badRequest } = require("../utils/reponsesHandler");
  * @param {Response} res
  */
 exports.addUser = function (req, res) {
-  const user = req.body;
-  User.create(user, (err, newUser) => {
-    if (err) {
-      console.log(err.errors);
-      const error = validationError(err);
-      return badRequest(res, error);
-    }
-    return successRequest(res, newUser);
-  });
+    const user = req.body;
+    User.create(user, (err, newUser) => {
+        if (err) {
+            console.log(err.errors);
+            const error = validationError(err);
+            return badResponse(res, error);
+        }
+        return successResponse(res, newUser);
+    });
 };
+
+/** Obtener todos los usuarios
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.getUsers = function (req, res) {
+    User.find((err, users) => {
+        if (err)
+            return internalServerErrorResponse(res, err);
+        if (users.length < 1)
+            return notFoundResponse(res, 'Users');
+        return successResponse(res, users);
+    });
+}
