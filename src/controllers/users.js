3a57506174415dc80422a1612591487e402d7711
Sergio Alberto Bernal Cisneros <sergiobernal909@gmail.com>
2020-12-29 20:22:56 -0600

    Skeleton delete user added

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -51,12 +51,33 @@ exports.updateUser = async function (req, res) {
     if (Object.keys(nUser).length <= 1 || nUser.constructor === {})
         return badResponse(res, { user: { msg: 'No fueron enviados los parametros necesarios' } });
     const { user_id } = nUser;
-    console.log('user_id: ', user_id);
     // Verificar si el id del usuario fu enviado
     if (!user_id)
         return badResponse(res, { user_id: { msg: 'No se recibio el id del usuario' } });
     const userExists = await existsRegister(User, user_id);
-    console.log('userExists', userExists);
+    // Verificar si el usuario existe
+    if (!userExists)
+        return notFoundResponse(res, `User -> ${user_id}`); 
+    User.findByIdAndUpdate(user_id, nUser, { new: true }, (err, result) => {
+        if(err)
+            return internalServerErrorResponse(res, err);
+        // Enviar el usuario actualizado
+        return successResponse(res, result);
+    });
+}
+
+/** Eliminar un usuario, es necesario que en req.body se envie la propiedad
+ * usuario_id, de lo contrario no se podra ejecutar la funcion update
+ * TODO: Implement delete method, but using an status value
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.deleteUser = async function(req, res) {
+    const { user_id } = req.body;
+    // Verificar si el id del usuario fu enviado
+    if (!user_id)
+        return badResponse(res, { user_id: { msg: 'No se recibio el id del usuario' } });
+    const userExists = await existsRegister(User, user_id);
     // Verificar si el usuario existe
     if (!userExists)
         return notFoundResponse(res, `User -> ${user_id}`); 
