0f1a9e0d58720c23ce556eaeeb880492fef66386
Sergio Alberto Bernal Cisneros <sergiobernal909@gmail.com>
2021-01-08 22:35:06 -0600

    delete, getbypost on comments done

--- src/models/comments.js
+++ src/models/comments.js
@@ -10,18 +10,22 @@ const commentSchema = new db.Schema({
     comment_created_at: { type: Date, default: Date.now },
     comment_updated_at: { type: Date, default: Date.now },
     comment_status: { type: Boolean, default: true },
-    /*comment_user: { 
+    comment_user: { 
         type: db.Schema.Types.ObjectId, 
         ref: 'users', 
         autopopulate: true,
         required: true
-    }*/
-    comment_1: Number,
-    comment_2: Number
+    },
+    comment_post: {
+        type: db.Schema.Types.ObjectId, 
+        ref: 'users', 
+        autopopulate: true,
+        required: true
+    }
 });
 
 commentSchema.plugin(require('mongoose-autopopulate'));
-commentSchema.index({ comment_1: 1, comment_2: 1 }, { unique: true });
+// commentSchema.index({ comment_1: 1, comment_2: 1 }, { unique: true });
 
 const _Comment = db.mongoose.model('comments', commentSchema);
 module.exports = _Comment;
